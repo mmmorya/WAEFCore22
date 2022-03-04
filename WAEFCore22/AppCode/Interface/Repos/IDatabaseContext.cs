0d60c8d80fe51d5343c4be3b4870e87aecc908f3
Mayank Morya <mmmorya@gmail.com>
2022-03-04 18:58:51 +0530

    UOW done

--- WAEFCore22/AppCode/Interface/Repos/IDatabaseContext.cs
+++ WAEFCore22/AppCode/Interface/Repos/IDatabaseContext.cs
@@ -3,12 +3,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace WAEFCore22.AppCode.Interface.Repos
 {
     public interface IDatabaseContext : IDisposable
     {
-        DbEntityEntry Entry(object entity);
+        EntityEntry Entry(object entity);
         DbSet<TEntity> Set<TEntity>() where TEntity : class;
         DbSet Set(Type entityType);
         int SaveChanges();
